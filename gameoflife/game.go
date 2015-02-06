a4698e7c148977accec74edda36be8be1d46b54e
Roel <nyubis@gmail.com>
2015-02-06 13:29:16 +0100

    Actual game of life functionality + beacon test

--- gameoflife/game.go
+++ gameoflife/game.go
@@ -1,24 +1,28 @@
 package gameoflife
 
 type Board struct {
-	w, h  int
-	cells [][]bool //A collection of rows, which contain cells
+	w, h   int
+	cells  [][]bool // A collection of rows, which contain cells
+	shadow [][]bool // Start as copy of cells, used as temp for calculating next step
 }
 
 func MakeBoard(width int, height int) *Board {
 	cells := make([][]bool, height)
+	shadow := make([][]bool, height)
 	for i := 0; i < height; i++ {
 		cells[i] = make([]bool, width)
+		shadow[i] = make([]bool, width)
 	}
 	return &Board{
-		w:     width,
-		h:     height,
-		cells: cells,
+		w:      width,
+		h:      height,
+		cells:  cells,
+		shadow: shadow,
 	}
 }
 
 func (board *Board) GetAt(x int, y int) bool {
-	if (x < 0 || y < 0 || x >= board.w || y >= board.h) {
+	if x < 0 || y < 0 || x >= board.w || y >= board.h {
 		return false
 	}
 	return board.cells[y][x]
@@ -29,8 +33,58 @@ func (board *Board) GetCells() [][]bool {
 }
 
 func (board *Board) SetAt(x int, y int, val bool) {
-	if (x < 0 || y < 0 || x >= board.w || y >= board.h) {
+	if x < 0 || y < 0 || x >= board.w || y >= board.h {
 		return // TODO: error reporting
 	}
 	board.cells[y][x] = val
+}
+
+func (board *Board) Step() {
+	for i, row := range board.cells {
+		for j, cell := range row {
+			n := board.countNeighbours(i,j)
+			board.shadow[i][j] = checkLive(n, cell)
+		}
+	}
+	board.shadow, board.cells = board.cells, board.shadow
+}
+
+func (board *Board) countNeighbours(row int, col int) int {
+	var count int
+	var leftbound, rightbound, upbound, lowbound int // In case the cell is at the edge of the board
+	leftbound, rightbound = calcBounds(col, board.w)
+	upbound, lowbound = calcBounds(row, board.h)
+
+	for i := upbound; i <= lowbound; i++ {
+		for j := leftbound; j <= rightbound; j++ {
+			if board.cells[i][j] && (row != i || col != j){
+				count++
+			}
+		}
+	}
+
+	return count
+}
+
+func calcBounds(val int, max int) (int, int) {
+	var minbound, maxbound int
+	if val == 0 {
+		minbound = 0
+	} else {
+		minbound = val - 1
+	}
+	if val == max - 1 {
+		maxbound = val
+	} else {
+		maxbound = val + 1
+	}
+	return minbound, maxbound
+}
+
+func checkLive(neighs int, isAlive bool) bool {
+	if neighs <= 1 || neighs >= 4 {
+		return false // Die of isolation or overpopulation
+	} else {
+		return isAlive || neighs == 3 // Survive or be reborn
+	}
 }
--- gameoflife/game_test.go
+++ gameoflife/game_test.go
@@ -8,6 +8,7 @@ type point struct {
 }
 
 func Test(t *testing.T) {
+	//Simple tests
 	b := MakeBoard(10, 10)
 	var tests = []struct {
 		give, want point
@@ -23,7 +24,23 @@ func Test(t *testing.T) {
 		b.SetAt(g.x, g.y, g.val)
 		got := b.GetAt(w.x, w.y)
 		if got != w.val {
-			t.Errorf("GetAt(%d, %d) == %b, want %b", w.x, w.y, w.val)
+			t.Errorf("GetAt(%d, %d) == %t, want %t", w.x, w.y, w.val)
 		}
+	}
+	//Beacon test
+	b = MakeBoard(4, 4)
+	b.SetAt(0,0,true)
+	b.SetAt(0,1,true)
+	b.SetAt(1,0,true)
+	b.SetAt(2,3,true)
+	b.SetAt(3,2,true)
+	b.SetAt(3,3,true)
+	for i := 0; i<5; i++ {
+		first := b.GetAt(1,1)
+		second := b.GetAt(2,2)
+		if (first != (i%2==1) || second != (i%2==1)) {
+			t.Errorf("Beacon iteration %d: cells are %t and %t", i, first, second)
+		}
+		b.Step()
 	}
 }
